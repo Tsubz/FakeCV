9d357e24a329a3a416cecd85020ffa2703833901
Tsubz <marcantoine.bazin@gmail.com>
2017-05-09 15:43:28 +0200

    Includes JS, show/hide "Social"

---
+++ script/MyScript.js
@@ -0,0 +1,10 @@
+
+function dubdub() {
+    if ($(".SectionDrop").is(":visible")) {
+        $(".SectionDrop").hide(1000);
+    }
+    else {
+        $(".SectionDrop").show(1000);
+    }
+    console.log("Clicked");
+}
